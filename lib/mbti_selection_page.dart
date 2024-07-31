7116adafa2ca116b43fa305fe1b55affa4da0630
Im HyeonJe <yusunju1357@gmail.com>
2024-08-01 02:44:50 +0900

    feat: 글자 중앙에 옮기기 실패!!!!!!

--- lib/mbti_selection_page.dart
+++ lib/mbti_selection_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'calendar_selection_page.dart'; // 달력 선택 페이지 파일을 import
 import 'loginpage.dart'; // 로그인 페이지 파일을 import
-import 'BestCourseTop3.dart'; // 새로운 파일 import
-import 'GyeongNamRecommend.dart'; // 새로운 파일 import
+import 'BestCourseTop3.dart'; // 인기 추천코스 파일을 import
+import 'GyeongNamRecommend.dart'; // 경상남도 행사 & 축제 파일을 import
 
 class MBTISelectionApp extends StatelessWidget {
   @override
@@ -37,26 +37,31 @@ class MBTISelectionPage extends StatefulWidget {
 }
 
 class _MBTISelectionPageState extends State<MBTISelectionPage> {
-  List<String> selectedMBTI = [];
+  Map<String, String?> selectedMBTI = {
+    'EI': null,
+    'SN': null,
+    'TF': null,
+    'JP': null,
+  };
   int _selectedIndex = 0;
 
-  void toggleSelection(String mbti) {
+  void toggleSelection(String group, String mbti) {
     setState(() {
-      if (selectedMBTI.contains(mbti)) {
-        selectedMBTI.remove(mbti);
-      } else if (selectedMBTI.length < 4) {
-        selectedMBTI.add(mbti);
-      }
+      selectedMBTI[group] = selectedMBTI[group] == mbti ? null : mbti;
     });
   }
 
+  bool allGroupsSelected() {
+    return selectedMBTI.values.every((element) => element != null);
+  }
+
   void navigateToCalendarPage() {
-    if (selectedMBTI.length == 4) {
+    if (allGroupsSelected()) {
       Navigator.push(
         context,
         MaterialPageRoute(
           builder: (context) => CalendarSelectionPage(
-            mbti: selectedMBTI.join(),
+            mbti: selectedMBTI.values.join(),
           ),
         ),
       );
@@ -128,8 +133,8 @@ class _MBTISelectionPageState extends State<MBTISelectionPage> {
                       borderRadius: BorderRadius.circular(20),
                     ),
                     child: Text(
-                      selectedMBTI.length == 4
-                          ? '${selectedMBTI.join()} OOO님!\n오늘은 경상남도 어디로 떠나볼까요?'
+                      allGroupsSelected()
+                          ? '${selectedMBTI.values.join()} OOO님!\n오늘은 경상남도 어디로 떠나볼까요?'
                           : '경상남도 추천지를 원하시면 MBTI를 선택해주세요 !!',
                       style: TextStyle(fontSize: screenHeight * 0.02),
                     ),
@@ -184,8 +189,8 @@ class _MBTISelectionPageState extends State<MBTISelectionPage> {
 }
 
 class StepOneWidget extends StatelessWidget {
-  final List<String> selectedMBTI;
-  final Function(String) toggleSelection;
+  final Map<String, String?> selectedMBTI;
+  final Function(String, String) toggleSelection;
   final VoidCallback onNextPressed;
 
   const StepOneWidget({
@@ -214,42 +219,50 @@ class StepOneWidget extends StatelessWidget {
               style: TextStyle(fontSize: screenHeight * 0.02),
               textAlign: TextAlign.left,
             ),
-            SizedBox(height: screenHeight * 0.01),
+            SizedBox(
+                height: screenHeight * 0.03), // 여기는 step1텍스트와 MBTI박스 간격 조절 부분
             Wrap(
               alignment: WrapAlignment.center,
-              spacing: screenHeight * 0.01,
-              runSpacing: screenHeight * 0.01,
+              spacing: screenHeight * 0.03, // mbti박스 간격 조절 부분
+
               children: [
                 MBTIGroup(
+                  group: 'EI',
                   verticalItems: ['E', 'I'],
                   selectedItems: selectedMBTI,
                   toggleSelection: toggleSelection,
                 ),
                 MBTIGroup(
+                  group: 'SN',
                   verticalItems: ['S', 'N'],
                   selectedItems: selectedMBTI,
                   toggleSelection: toggleSelection,
                 ),
                 MBTIGroup(
+                  group: 'TF',
                   verticalItems: ['T', 'F'],
                   selectedItems: selectedMBTI,
                   toggleSelection: toggleSelection,
                 ),
                 MBTIGroup(
+                  group: 'JP',
                   verticalItems: ['J', 'P'],
                   selectedItems: selectedMBTI,
                   toggleSelection: toggleSelection,
                 ),
               ],
             ),
-            SizedBox(height: screenHeight * 0.03),
+            SizedBox(height: screenHeight * 0.03), //요놈은 '다음'박스랑 MBTI박스사이 거리
             ElevatedButton(
-              onPressed: selectedMBTI.length == 4 ? onNextPressed : null,
+              onPressed: allGroupsSelected() ? onNextPressed : null,
               child: Text('다음'),
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.grey[800],
-                foregroundColor: Colors.white,
-                padding: EdgeInsets.symmetric(vertical: screenHeight * 0.02),
+                foregroundColor: allGroupsSelected()
+                    ? Colors.white
+                    : Colors.white, // 비활성화 색상 설정
+                padding: EdgeInsets.symmetric(
+                    vertical: screenHeight * 0.02), //이건 '다음'이랑 박스내부 비율
               ),
             ),
           ],
@@ -257,14 +270,20 @@ class StepOneWidget extends StatelessWidget {
       ),
     );
   }
+
+  bool allGroupsSelected() {
+    return selectedMBTI.values.every((element) => element != null);
+  }
 }
 
 class MBTIGroup extends StatelessWidget {
+  final String group;
   final List<String> verticalItems;
-  final List<String> selectedItems;
-  final Function(String) toggleSelection;
+  final Map<String, String?> selectedItems;
+  final Function(String, String) toggleSelection;
 
   const MBTIGroup({
+    required this.group,
     required this.verticalItems,
     required this.selectedItems,
     required this.toggleSelection,
@@ -279,12 +298,13 @@ class MBTIGroup extends StatelessWidget {
         color: Colors.white,
         borderRadius: BorderRadius.circular(10),
       ),
-      padding: EdgeInsets.all(screenHeight * 0.01),
+      padding: EdgeInsets.all(screenHeight * 0.01), //이건 뭔가 박스랑 글자사이 간격비율같은데...
       child: Column(
         children: verticalItems.map((mbti) {
           return MBTIButton(
+            group: group,
             mbti: mbti,
-            isSelected: selectedItems.contains(mbti),
+            isSelected: selectedItems[group] == mbti,
             toggleSelection: toggleSelection,
           );
         }).toList(),
@@ -294,11 +314,13 @@ class MBTIGroup extends StatelessWidget {
 }
 
 class MBTIButton extends StatelessWidget {
+  final String group;
   final String mbti;
   final bool isSelected;
-  final Function(String) toggleSelection;
+  final Function(String, String) toggleSelection;
 
   const MBTIButton({
+    required this.group,
     required this.mbti,
     required this.isSelected,
     required this.toggleSelection,
@@ -309,20 +331,31 @@ class MBTIButton extends StatelessWidget {
     final double screenHeight = MediaQuery.of(context).size.height;
 
     return Container(
-      width: screenHeight * 0.07,
-      height: screenHeight * 0.07,
+      width: screenHeight * 0.08, //이건 세로박스 너비
+      height: screenHeight * 0.08, //이건 세로박스 높이
       margin: EdgeInsets.all(screenHeight * 0.004),
-      child: ElevatedButton(
-        onPressed: () => toggleSelection(mbti),
-        child: Text(mbti, style: TextStyle(fontSize: screenHeight * 0.025)),
-        style: ElevatedButton.styleFrom(
-          backgroundColor: isSelected ? Colors.grey[300] : Colors.white,
-          foregroundColor: Colors.black,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-            side: BorderSide(color: Colors.grey),
+      child: Center(
+        child: ElevatedButton(
+          onPressed: () => toggleSelection(group, mbti),
+          // 여기에 Padding을 추가하여 텍스트와 박스 사이의 간격을 조절합니다.
+          child: Padding(
+            padding: EdgeInsets.all(
+                screenHeight * 0.0000005), // 글자와 박스 사이의 간격을 조절합니다.
+            child: Text(
+              mbti,
+              style: TextStyle(fontSize: screenHeight * 0.05), //이건 MBTI 글자크기
+              textAlign: TextAlign.center, // 텍스트를 중앙 정렬합니다.
+            ),
           ),
-          elevation: 1,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: isSelected ? Colors.grey[300] : Colors.white,
+            foregroundColor: Colors.black,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+              side: BorderSide(color: Colors.white), // 경계선을 흰색으로 설정
+            ),
+            elevation: 0, // 음영 제거
+          ),
         ),
       ),
     );
