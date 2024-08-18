0c8e8e8b0b9fc420d7c06e0a10c7594a3d3c0695
Chaeyeoncho <gcy9293@naver.com>
2024-08-19 00:46:19 +0900

    feat: 파일 제거

--- lib/hooks/travelplan.dart
+++ lib/hooks/travelplan.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_svg/svg.dart';
 import '../components/basic_frame_page.dart';
 
 class MyNewPage extends StatefulWidget {
