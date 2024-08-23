3c01fb6da0f9c61a8e0ba88119bc778a01e5feaa
Chaeyeoncho <gcy9293@naver.com>
2024-08-24 02:15:57 +0900

    feat : 카카오 맵 설정

--- lib/services/map.dart
+++ lib/services/map.dart
@@ -3,32 +3,32 @@ import 'package:flutter_kakao_map/flutter_kakao_map.dart';
 import 'package:flutter_kakao_map/kakao_maps_flutter_platform_interface.dart';
 
 void main() {
- runApp(MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
- @override
- Widget build(BuildContext context) {
-  return MaterialApp(
-   title: 'KaKao Map',
-   home: KakaoMapTest(),
-  );
- }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'KaKao Map',
+      home: KakaoMapTest(),
+    );
+  }
 }
 
 class KakaoMapTest extends StatefulWidget {
- @override
- _KakaoMapTestState createState() => _KakaoMapTestState();
+  @override
+  _KakaoMapTestState createState() => _KakaoMapTestState();
 }
 
 class _KakaoMapTestState extends State<KakaoMapTest> {
- @override
- Widget build(BuildContext context) {
-  return Scaffold(
-   appBar: AppBar(title: const Text('Flutter KakaoMap example')),
-   body: Column(
-​    children: [],
-   ),
-  );
- }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter KakaoMap example')),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
 }
