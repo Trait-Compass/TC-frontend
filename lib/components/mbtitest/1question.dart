b44bae229c5486cb55fb0d721099d34e2f73634f
Chaeyeoncho <gcy9293@naver.com>
2024-08-26 16:04:43 +0900

    feat : mbti 테스트 첫 질문 페이지

---
+++ lib/components/mbtitest/1question.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+class TravelPreferencePage extends StatelessWidget {
+  final Function(String) onOptionSelected; // 선택된 옵션을 전달하는 콜백 함수
+  final String selectedOption; // 현재 선택된 옵션
+
+  TravelPreferencePage({
+    required this.onOptionSelected,
+    required this.selectedOption,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          '여행을 가고 싶은 곳은?',
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: GestureDetector(
+                onTap: () => onOptionSelected('nature'),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        blurRadius: 10,
+                        offset: Offset(0, 5),
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Container(
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: selectedOption == 'nature'
+                            ? Colors.grey[300]
+                            : Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            '../assets/nature.png',
+                            height: 60,
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            '한적한 자연',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 20),
+            Expanded(
+              child: GestureDetector(
+                onTap: () => onOptionSelected('city'),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        blurRadius: 10,
+                        offset: Offset(0, 5),
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Container(
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: selectedOption == 'city'
+                            ? Colors.grey[300]
+                            : Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            '../assets/city.png',
+                            height: 60,
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            '신나는 도시',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: () {
+            // 다음 버튼을 눌렀을 때의 로직 추가
+          },
+          child: Text(
+            '다음',
+            style: TextStyle(fontSize: 18, color: Colors.white),
+          ),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.grey,
+            padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
