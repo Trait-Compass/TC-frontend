767cc19680b0a2fdbacc834099cacc7ef6b4b6d0
Chaeyeoncho <gcy9293@naver.com>
2024-08-26 12:48:29 +0900

    feat : mbti 테스트 페이지

--- lib/hooks/MBTItestpage.dart
+++ lib/hooks/MBTItestpage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../components/basic_frame_page.dart'; // BasicFramePage 임포트
+import '../components/basicframe.dart'; // BasicFramePage 임포트
 
 // StatefulWidget 구현
 class MBTItestselection extends StatefulWidget {
@@ -11,103 +11,141 @@ class MBTItestselection extends StatefulWidget {
 class _MBTItestselectionState extends State<MBTItestselection> {
   @override
   Widget build(BuildContext context) {
-    return BasicFramePage(
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    return BasicFrame1Page(
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 20),
-            // 상단 섹션: MBTI 테스트 버튼
-            Center(
-              child: Column(
-                children: [
-                  Text(
-                    '나의 여행 MBTI는?',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Divider(
+                color: Color(0xFFE4E4E4),
+                thickness: 1,
+                height: 1,
+              ),
+              SizedBox(height: 20),
+              Text(
+                '나의 여행 MBTI는?',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: screenWidth - 40, // 화면에 꽉 차도록 설정 (20px * 2 = 40px)
+                height: 300,
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
                   ),
-                  SizedBox(height: 20),
-                  GestureDetector(
-                    onTap: () {
-                      // 임시로 페이지 이동하지 않도록 설정
-                      // 예시: Navigator.push(context, MaterialPageRoute(builder: (context) => AnotherPage()));
-                    },
-                    child: Container(
-                      padding: EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.blue, // 테두리 색상 설정
-                          width: 2,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Column(
-                        children: [
-                          Image.asset('assets/airplane_icon.png',
-                              height: 100), // 비행기 아이콘 이미지 추가
-                          SizedBox(height: 10),
-                          Text(
-                            'MBTI 테스트',
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {},
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.zero,
+                              child: Image.asset(
+                                '../assets/airplane.png',
+                                height: 50,
+                                fit: BoxFit.contain, // 이미지를 꽉 채우기 위해 사용
+                              ),
                             ),
+                            Padding(
+                              padding: EdgeInsets.zero,
+                              child: Text(
+                                'MBTI 테스트',
+                                style: TextStyle(
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold,
+                                  height: 1.0,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.zero,
+                              child: Image.asset(
+                                '../assets/airplane2.png',
+                                height: 40,
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          '시작하기',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
                           ),
-                        ],
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.grey,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 50, vertical: 15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              // 회색 박스 안의 내용 (마스코트)
+              Container(
+                width: screenWidth - 40,
+                height: 400,
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('../assets/mascoat.png', height: 100),
+                    SizedBox(height: 20),
+                    Text(
+                      '안녕하세요 창녕군 마스코트 따오기예요!',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  ElevatedButton(
-                    onPressed: () {
-                      // 여기도 임시로 페이지 이동하지 않도록 설정
-                    },
-                    child: Text('시작하기'),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.grey,
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
+                    SizedBox(height: 10),
+                    Text(
+                      'MBTI를 먼저 입력해주세요',
+                      style: TextStyle(fontSize: 16),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 30),
-            // 중간 섹션: 마스코트와 설명 텍스트
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Column(
-                children: [
-                  // 마스코트 이미지
-                  Image.asset('assets/mascot.png', height: 100),
-                  SizedBox(height: 20),
-                  Text(
-                    '안녕하세요 창녕군 마스코트 따오기예요!',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+                    SizedBox(height: 10),
+                    Text(
+                      '입력 후, 다른 MBTI의 성향도 볼 수 있어요!',
+                      style: TextStyle(fontSize: 16),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    'MBTI를 먼저 입력해주세요',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    '입력 후, 다른 MBTI의 성향도 볼 수 있어요!',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 30),
-          ],
+              SizedBox(height: 30),
+            ],
+          ),
         ),
       ),
     );
