693c8c59168394e17283143d496cb5799ee1762a
Chaeyeoncho <gcy9293@naver.com>
2024-08-30 03:36:42 +0900

    fix : splash 디자인 수정

--- lib/components/start/splash.dart
+++ lib/components/start/splash.dart
@@ -17,18 +17,19 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset(
               'assets/mbti.png',
-              height: 150,
+              height: 60,
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 10),
             Text(
               'MBTI로 떠나는 여행',
-              style: TextStyle(fontSize: 20, color: Colors.grey[200]),
+              style: TextStyle(fontSize: 20, color: Colors.grey[500]),
             ),
           ],
         ),
