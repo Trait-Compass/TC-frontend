c70e410abf26fbd0a214c88a656b2d558ba0480e
Chaeyeoncho <gcy9293@naver.com>
2024-08-30 03:02:49 +0900

    feat : splash 구현

---
+++ lib/components/start/splash.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, '/onboarding');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/splash_image.png',
+              height: 150,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'MBTI로 떠나는 여행',
+              style: TextStyle(fontSize: 20, color: Colors.grey[200]),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
