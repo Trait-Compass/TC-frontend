cd3dc7a43ddc5c0dd0e25ebc300c70f834f2fd77
Chaeyeoncho <gcy9293@naver.com>
2024-09-09 02:09:47 +0900

    fix : 배경 변경

--- lib/components/basic_frame_page.dart
+++ lib/components/basic_frame_page.dart
@@ -46,25 +46,23 @@ class _BasicFramePageState extends State<BasicFramePage> {
     final double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(screenHeight * 0.08),
-        child: Container(
-          color: Colors.white,
-          child: AppBar(
-            centerTitle: true,
-            title:
-                Image.asset('assets/mbtilogo.jpg', height: screenHeight * 0.05),
-            backgroundColor: Colors.white,
-            elevation: 0,
-            actions: [
-              IconButton(
-                icon: Image.asset('assets/alarm.jpg'),
-                onPressed: () {},
-              ),
-            ],
+      backgroundColor: Colors.white, 
+      appBar: AppBar(
+        centerTitle: true,
+        title: Image.asset('assets/mbtilogo.jpg', height: screenHeight * 0.05),
+        backgroundColor: Colors.white, 
+        elevation: 0, 
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            color: Colors.white, 
           ),
         ),
+        actions: [
+          IconButton(
+            icon: Image.asset('assets/alarm.jpg'),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: widget.body,
       bottomNavigationBar: BottomNavigationBar(
