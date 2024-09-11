7f6f7f7d486ddfa1d8a358e33acfb027045c1b51
Chaeyeoncho <gcy9293@naver.com>
2024-09-11 12:38:41 +0900

    fix : 중복 basicframe 제거

--- lib/components/Mypage/myprofilefirstpage.dart
+++ lib/components/Mypage/myprofilefirstpage.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-
-import '../basic_frame_page.dart'; // 실제 경로는 프로젝트 구조에 따라 다를 수 있습니다.
 import '../Mypage/myprofile.dart';
 import '../Mypage/savedcourses.dart';
 import '../Mypage/traveldiary.dart';
@@ -15,7 +13,8 @@ void main() {
 class Mypage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BasicFramePage(
+    return Scaffold(
+      backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -24,7 +23,7 @@ class Mypage extends StatelessWidget {
             SizedBox(height: 5),
             ProfileSection(),
             SizedBox(height: 10),
-            SavedTravelCourses(), // RadarChart 추가
+            SavedTravelCourses(), 
             SizedBox(height: 10),
             TravelDiary(),
             SizedBox(height: 10),
--- lib/pages/travelplan.dart
+++ lib/pages/travelplan.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/pages/Jselection.dart';
 import 'package:untitled/pages/Pselection.dart';
-import '../components/basic_frame_page.dart';
-import '../hooks/top3course.dart'; // 추가된 Top3Courses 파일 import
+import '../hooks/top3course.dart'; 
 
 class MyNewPage extends StatefulWidget {
   @override
@@ -20,7 +19,8 @@ class _MyNewPageState extends State<MyNewPage> {
 
   @override
   Widget build(BuildContext context) {
-    return BasicFramePage(
+    return Scaffold(
+      backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
