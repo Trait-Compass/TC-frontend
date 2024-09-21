490228078862b0455b3494a0515fa21f5d601676
Chaeyeoncho <gcy9293@naver.com>
2024-09-21 23:24:30 +0900

    fix : StatefulWidget으로 변경 : 코스 재생성 이미지만

--- lib/pages/result_page.dart
+++ lib/pages/result_page.dart
@@ -65,38 +65,10 @@ class ResultPage extends StatelessWidget {
                 color: Colors.grey[200],
                 borderRadius: BorderRadius.circular(20),
               ),
-              child: Column(
-                children: [
-                  RecommendedCourses(
-                    mbti: mbti,
-                    startDate: startDate,
-                    endDate: endDate,
-                  ),
-                  SizedBox(height: 10),
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ResultPage(
-                              mbti: mbti,
-                              startDate: startDate,
-                              endDate: endDate,
-                            ),
-                          ),
-                        );
-                      },
-                      child: Text('코스 재생성'),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.grey[800],
-                        foregroundColor: Colors.white,
-                        padding:
-                            EdgeInsets.symmetric(vertical: 5, horizontal: 30),
-                      ),
-                    ),
-                  ),
-                ],
+              child: RecommendedCourses(
+                mbti: mbti,
+                startDate: startDate,
+                endDate: endDate,
               ),
             ),
             SizedBox(height: 20),
@@ -111,7 +83,7 @@ class ResultPage extends StatelessWidget {
   }
 }
 
-class RecommendedCourses extends StatelessWidget {
+class RecommendedCourses extends StatefulWidget {
   final String mbti;
   final DateTime startDate;
   final DateTime endDate;
@@ -122,58 +94,105 @@ class RecommendedCourses extends StatelessWidget {
     required this.endDate,
   });
 
-  Future<List<Course>> fetchCourses() async {
-    final String formattedStartDate = DateFormat('yyyy-MM-dd').format(startDate);
-    final String formattedEndDate = DateFormat('yyyy-MM-dd').format(endDate);
+  @override
+  _RecommendedCoursesState createState() => _RecommendedCoursesState();
+}
 
-  
+class _RecommendedCoursesState extends State<RecommendedCourses> {
+  late Future<List<Course>> _coursesFuture;
+  List<Course> _displayCourses = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _coursesFuture = fetchCourses();
+  }
+
+  Future<List<Course>> fetchCourses() async {
+    final String formattedStartDate = DateFormat('yyyy-MM-dd').format(widget.startDate);
+    final String formattedEndDate = DateFormat('yyyy-MM-dd').format(widget.endDate);
+
     final Uri uri = Uri.parse('https://www.traitcompass.store/course/simple').replace(queryParameters: {
-      'mbti': mbti,
+      'mbti': widget.mbti,
       'startDate': formattedStartDate,
       'endDate': formattedEndDate,
     });
 
-    print('Requesting URL: $uri'); // 디버그 메시지 추가
-
     final response = await http.get(uri);
-
-    print('Response status code: ${response.statusCode}');
-    print('Response body: ${response.body}');
 
     if (response.statusCode == 200) {
       List jsonResponse = json.decode(response.body)['result'];
-      return jsonResponse.map((course) => Course.fromJson(course)).toList();
+      List<Course> courses = jsonResponse.map((course) => Course.fromJson(course)).toList();
+
+      // 코스 리스트를 상태에 저장하고, 처음 4개를 표시할 코스로 선택
+      setState(() {
+        _displayCourses = getRandomCourses(courses);
+      });
+
+      return courses;
     } else {
       throw Exception('코스를 불러오는 중입니다! 잠시만 기다려주세요!');
     }
   }
 
+
+  List<Course> getRandomCourses(List<Course> courses) {
+    courses.shuffle();
+    int count = courses.length >= 4 ? 4 : courses.length;
+    return courses.take(count).toList();
+  }
+
+  void regenerateCourses(List<Course> courses) {
+    setState(() {
+      _displayCourses = getRandomCourses(courses);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<Course>>(
-      future: fetchCourses(),
+      future: _coursesFuture,
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Center(child: CircularProgressIndicator());
         } else if (snapshot.hasError) {
-          return Center(child: Text('Failed to load courses'));
+          return Center(child: Text('코스를 불러오는 중입니다! 잠시만 기다려주세요!'));
         } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-          return Center(child: Text('No courses available'));
+          return Center(child: Text('이용 가능한 코스가 없습니다'));
         } else {
-          List<Course> courses = snapshot.data!;
-          return GridView.builder(
-            physics: NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, 
-              crossAxisSpacing: 10, 
-              mainAxisSpacing: 10, 
-              childAspectRatio: 1, 
-            ),
-            itemCount: courses.length,
-            itemBuilder: (context, index) {
-              return CourseCard(course: courses[index]);
-            },
+          List<Course> allCourses = snapshot.data!;
+
+          return Column(
+            children: [
+              GridView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, 
+                  crossAxisSpacing: 10, 
+                  mainAxisSpacing: 10, 
+                  childAspectRatio: 1, 
+                ),
+                itemCount: _displayCourses.length,
+                itemBuilder: (context, index) {
+                  return CourseCard(course: _displayCourses[index]);
+                },
+              ),
+              SizedBox(height: 10),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    regenerateCourses(allCourses);
+                  },
+                  child: Text('코스 재생성'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey[800],
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 30),
+                  ),
+                ),
+              ),
+            ],
           );
         }
       },
@@ -181,6 +200,7 @@ class RecommendedCourses extends StatelessWidget {
   }
 }
 
+// Course 클래스 정의
 class Course {
   final String region;
   final String courseName;
@@ -204,6 +224,7 @@ class Course {
   }
 }
 
+// CourseCard 위젯 정의
 class CourseCard extends StatelessWidget {
   final Course course;
 
@@ -213,8 +234,8 @@ class CourseCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
+        // 코스 상세 페이지로 이동하거나 다른 동작을 수행할 수 있습니다.
       },
-
       child: Card(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
         elevation: 5, 
@@ -249,6 +270,7 @@ class CourseCard extends StatelessWidget {
                   },
                 ),
               ),
+              // 그라데이션 오버레이
               Positioned.fill(
                 child: Container(
                   decoration: BoxDecoration(
@@ -263,6 +285,7 @@ class CourseCard extends StatelessWidget {
                   ),
                 ),
               ),
+              // 텍스트 정보
               Positioned(
                 bottom: 10,
                 left: 10,
