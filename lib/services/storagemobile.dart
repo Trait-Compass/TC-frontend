5aeb0726e132c3e93242c6a538c995056d975c85
Chaeyeoncho <gcy9293@naver.com>
2024-09-29 00:58:52 +0900

    fix : 웹 로그인 적용

--- lib/services/storagemobile.dart
+++ lib/services/storagemobile.dart
@@ -2,22 +2,41 @@
 
 import 'storageservice.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/foundation.dart'; 
 
 class StorageService implements StorageServiceBase {
   final FlutterSecureStorage _secureStorage = FlutterSecureStorage();
 
   @override
   Future<void> write({required String key, required String value}) async {
-    await _secureStorage.write(key: key, value: value);
+    if (kIsWeb) {
+
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString(key, value);
+    } else {
+
+      await _secureStorage.write(key: key, value: value);
+    }
   }
 
   @override
   Future<String?> read({required String key}) async {
-    return await _secureStorage.read(key: key);
+    if (kIsWeb) {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      return prefs.getString(key);
+    } else {
+      return await _secureStorage.read(key: key);
+    }
   }
 
   @override
   Future<void> delete({required String key}) async {
-    await _secureStorage.delete(key: key);
+    if (kIsWeb) {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.remove(key);
+    } else {
+      await _secureStorage.delete(key: key);
+    }
   }
 }
